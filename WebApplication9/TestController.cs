0a86ad775eead623bcd697d70eb247f84aeb35f7
Bart calixto <bart@bartmax.com>
2017-07-28 15:38:21 -0300

    initial commit

---
+++ WebApplication9/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication9
+{
+    public class TestController : Controller
+    {
+        [HttpGet("/wtf")]
+        public async Task<IActionResult> WTF()
+        {
+             var x = TempData.ContainsKey("SomeKey");
+            await Task.Run(() => throw new Exception("error"));
+            return Content("this will never hit.");
+        }
+    }
+}
